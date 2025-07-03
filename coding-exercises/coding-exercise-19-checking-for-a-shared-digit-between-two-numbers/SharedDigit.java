3b60b5208fc944badb83814b5e75f8f582c25d05
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-03 06:06:12 +0300

    Coding Exercise 19: Checking For A Shared Digit Between Two Numbers

---
+++ coding-exercises/coding-exercise-19-checking-for-a-shared-digit-between-two-numbers/SharedDigit.java
@@ -0,0 +1,25 @@
+public class SharedDigit {
+    // write code here
+       public static boolean hasSharedDigit(int num1, int num2) {
+        if (num1 < 10 || num1 > 99 || num2 < 10 || num2 > 99)
+            return false;
+
+        int temp1 = num1;
+
+        while (temp1 != 0) {
+            int b = temp1 % 10;
+            int temp2 = num2; 
+            while (temp2 != 0) {
+                int a = temp2 % 10;
+                System.out.println("temp 1 = " + b );
+                System.out.println("temp 2 = " + a );
+                if (a == b)
+                    return true;
+                temp2 /= 10;
+            }
+            temp1 /= 10;
+        }
+        return false;
+    }
+
+}
--- titleConverter.py
+++ titleConverter.py
@@ -47,7 +47,7 @@ def create_code_exercise(section_title, part_title):
 # section_title = "Section 6: Mastering Conditional Logic, Loops, And Interactive Java Applications"
 section_title = "coding-exercises"
 
-part_title = "Coding Exercise 18: Calculating The Sum Of All Even Digits In An Integer"
+part_title = "Coding Exercise 19: Checking For A Shared Digit Between Two Numbers"
 
 # prepare_resources(section_title, part_title)
 create_code_exercise(section_title, part_title)
