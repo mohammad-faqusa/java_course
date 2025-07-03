4976f14db11485c35c3c357a034799aace9ee139
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-03 05:02:49 +0300

    70. Understanding Classes, Objects, And Static Versus Instance Members

--- titleConverter.py
+++ titleConverter.py
@@ -47,7 +47,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 6: Mastering Conditional Logic, Loops, And Interactive Java Applications"
 # section_title = "coding-exercises"
 
-part_title = "69. Exploring Local Variables And Scope In Java Blocks"
+part_title = "70. Understanding Classes, Objects, And Static Versus Instance Members"
 
 prepare_resources(section_title, part_title)
 # create_code_exercise(section_title, part_title)
