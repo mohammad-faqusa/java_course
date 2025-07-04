56691dd4c1b7d9fb517c9df19f5efbef55ff95ac
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-04 12:06:16 +0300

    Coding Exercise 31: Person Class and Age Validation

--- titleConverter.py
+++ titleConverter.py
@@ -47,7 +47,7 @@ def create_code_exercise(section_title, part_title):
 # section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
 section_title = "coding-exercises"
 
-part_title = "Coding Exercise 30: Sum Calculator Implementation"
+part_title = "Coding Exercise 31: Person Class and Age Validation"
 
 # prepare_resources(section_title, part_title)
 create_code_exercise(section_title, part_title)
