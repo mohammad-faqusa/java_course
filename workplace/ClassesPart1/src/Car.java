d4ed6fcc77121e60a323b94d38fdc612c17569b1
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-04 11:05:46 +0300

    78. Getters, Encapsulation, and Object Access

--- workplace/ClassesPart1/src/Car.java
+++ workplace/ClassesPart1/src/Car.java
@@ -1,12 +1,32 @@
 public class Car {
 
-    private String make;
-    private String model;
-    private String color;
+    private String make ;
+    private String model ;
+    private String color ;
     private int doors;
-    private boolean convertible;
+    private boolean convertible ;
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public void describeCar(){
-        System.out.println(doors + "-Door "+ model + "-Model " + color + "-Color " + make + "-Make " + (convertible ? "Convertable" : "Non-Convertable") );
+        System.out.println(doors + "-Door "+ model + "-Model " + color + "-Color " + make + "-Make " + (convertible ? "Convertable" : "") );
     }
 }
