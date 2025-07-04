4f45f9cde746cc8573a9ea566a53fd9fd7b0c12d
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-04 11:57:25 +0300

    Coding Exercise 30: Sum Calculator Implementation

--- titleConverter.py
+++ titleConverter.py
@@ -44,10 +44,10 @@ def create_code_exercise(section_title, part_title):
     # print(part_directory_path)
     create_directory(part_directory_path)
 
-section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
-# section_title = "coding-exercises"
+# section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
+section_title = "coding-exercises"
 
-part_title = "80. Classes Challenge: Building a Bank Account"
+part_title = "Coding Exercise 30: Sum Calculator Implementation"
 
-prepare_resources(section_title, part_title)
-# create_code_exercise(section_title, part_title)
+# prepare_resources(section_title, part_title)
+create_code_exercise(section_title, part_title)
