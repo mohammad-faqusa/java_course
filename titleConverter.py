23ecaeb805f7d75b62baf593f197d2e612d2892c
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-05 09:55:26 +0300

    Coding Exercise 34: Carpet Cost Calculator Class

--- titleConverter.py
+++ titleConverter.py
@@ -47,7 +47,7 @@ def create_code_exercise(section_title, part_title):
 # section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
 section_title = "coding-exercises"
 
-part_title = "Coding Exercise 33: Point Class and Distance Calculation"
+part_title = "Coding Exercise 34: Carpet Cost Calculator Class"
 
 # prepare_resources(section_title, part_title)
 create_code_exercise(section_title, part_title)
