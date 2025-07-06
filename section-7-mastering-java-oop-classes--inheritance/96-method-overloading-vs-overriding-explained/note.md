a1e61cc862447c963cb9cf582e35b2f58e87a90c
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-06 05:57:39 +0300

    96. Method Overloading vs. Overriding Explained

---
+++ section-7-mastering-java-oop-classes--inheritance/96-method-overloading-vs-overriding-explained/note.md
@@ -0,0 +1,9 @@
+## method overloading vs method overriding
+
+we can overload static or instance metods
+
+Java developers often refere to method **overloading as** compile-time polymorphism
+
+while **method overloading** is runtime polymorphism
+
+![1751770078880](image/note/1751770078880.png)
--- titleConverter.py
+++ titleConverter.py
@@ -47,7 +47,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
 # section_title = "coding-exercises"
 
-part_title = "95. Using this and super for Constructors"
+part_title = "96. Method Overloading vs. Overriding Explained"
 
 prepare_resources(section_title, part_title)
 # create_code_exercise(section_title, part_title)
