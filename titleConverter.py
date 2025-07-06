03e0a1873352698f19e4e87be340d4e17f011580
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-06 08:12:57 +0300

    Coding Exercise 36: Cylinder Class with Circle Inheritance

--- titleConverter.py
+++ titleConverter.py
@@ -44,10 +44,10 @@ def create_code_exercise(section_title, part_title):
     # print(part_directory_path)
     create_directory(part_directory_path)
 
-section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
-# section_title = "coding-exercises"
+# section_title = "Section 7: Mastering Java OOP: Classes & Inheritance"
+section_title = "coding-exercises"
 
-part_title = "100. The StringBuilder: Efficient Mutable Strings"
+part_title = "Coding Exercise 36: Cylinder Class with Circle Inheritance"
 
-prepare_resources(section_title, part_title)
-# create_code_exercise(section_title, part_title)
+# prepare_resources(section_title, part_title)
+create_code_exercise(section_title, part_title)
